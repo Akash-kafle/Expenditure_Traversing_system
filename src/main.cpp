c059e59444bed9effd23ea4cd2055a32ca03e8a9
Sammit <poudyal.sammit@gmail.com>
2023-12-31 00:25:44 +0545

    changed gif

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,19 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Login w;
-    //QFontDatabase::addApplicationFont("C:/Users/97798/Desktop/QTP/Expenditure_Traversing_system/Sf-Pro.ttf");
-    //QFontDatabase db;
-    //for(int i=0; i<db.families().size(); i++)
-    //{
-    //    qDebug() << db.families().at(i);
-    //}
-
-    QMovie *movie = new QMovie("C:/Users/97798/Desktop/QTP/Expenditure_Traversing_system/img/splash_screen.gif");
+    QMovie *movie = new QMovie("C:/Users/97798/Desktop/QTP1/Expenditure_Traversing_system/img/splash_screen11.gif");
     QLabel *processLabel = new QLabel(NULL);
-    processLabel->resize(675,450);
+    processLabel->resize(700,480);
     processLabel->setMovie(movie);
     movie->start();
-    processLabel->setWindowIcon(QIcon("C:/Users/97798/Desktop/QTP/Expenditure_Traversing_system/img/logo.ico"));
+    processLabel->setWindowIcon(QIcon("C:/Users/97798/Desktop/QTP1/Expenditure_Traversing_system/img/logo.ico"));
     processLabel->setWindowFlags(Qt::FramelessWindowHint);
     processLabel->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, processLabel->size(), qApp->desktop()->availableGeometry()/*QGuiApplication::screens()*/));
     processLabel->show();
     w.setWindowTitle("Expenditure Traversing System");
 
-    QTimer::singleShot(2500, processLabel, SLOT(close()));
-    QTimer::singleShot(2500, &w, SLOT(show()));
+    QTimer::singleShot(2700, processLabel, SLOT(close()));
+    QTimer::singleShot(2700, &w, SLOT(show()));
 
     return a.exec();
 }
